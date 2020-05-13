cfdc5dee652cac71d6f657f8bd3c6723ced20377
Kanika Sharma <kanikash4@gmail.com>
2020-05-13 14:29:54 +0530

    Adding cabs info

---
+++ src/server/data/cab.js
@@ -0,0 +1,46 @@
+module.exports = [
+  {
+    "id": 1,
+    "driverName": "driver 1",
+    "driverNumber": "001",
+    "location": {
+      "lattitude": 0,
+      "longitude": 0
+    },
+    "isBooked": false,
+    "color": "WHITE"
+  },
+  {
+    "id": 2,
+    "driverName": "driver 2",
+    "driverNumber": "002",
+    "location": {
+      "lattitude": 100,
+      "longitude": 100
+    },
+    "isBooked": false,
+    "color": "PINK"
+  },
+  {
+    "id": 3,
+    "driverName": "driver 3",
+    "driverNumber": "003",
+    "location": {
+      "lattitude": 100,
+      "longitude": 200
+    },
+    "isBooked": false,
+    "color": "PINK"
+  },
+  {
+    "id": 4,
+    "driverName": "driver 4",
+    "driverNumber": "004",
+    "location": {
+      "lattitude": 200,
+      "longitude": 100
+    },
+    "isBooked": false,
+    "color": "WHITE"
+  }
+];
