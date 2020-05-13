a6b0c2bd722bba5ff01daa2c412ea9dcab4f578c
Kanika Sharma <kanikash4@gmail.com>
2020-05-13 14:35:14 +0530

    Removed unwanted data

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -2,12 +2,9 @@
 const express = require('express');
 const router = express.Router();
 const cabs = require('../controllers/cabBooking');
-const home = require('../controllers/home');
 const allCabs = require('../data/cab');
 
 router.get('/_status', function (req, res, next) { res.sendStatus(200);});
-
-router.get('/', home.index);
 
 router.get('/showall', function(req, res, next) {
   res.json({
